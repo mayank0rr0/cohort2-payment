f2fafa4e0feb980d5e06e1ec13fc1304b557cafa
Mayank K <mayank0rr0@gmail.com>
2025-11-06 04:18:47 +0000

    Added redirect logic

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,18 @@
 import { PageHead } from "@repo/ui/PageHead"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../lib/auth";
+import { redirect } from "next/navigation";
 
-export default  function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+
+  // OF NO USE THE USER WILL BE REDIRECTED ANYHOW
   return (
   <div>
     <div className="flex justify-center text-center bg-amber-300">
