acbdd4ec97d1312c7a9333256d29f947bbe6cc4b
Mayank K <mayank0rr0@gmail.com>
2025-11-08 03:13:09 +0000

    Added p2pTransfer server action

--- apps/user-app/lib/actions/createP2PTransfer.ts
+++ apps/user-app/lib/actions/createP2PTransfer.ts
@@ -1,19 +1,72 @@
  /// FILLL this up
 
 import { prisma } from "@repo/db";
+import type { Balance } from "@repo/db";
 import { getServerSession } from "next-auth";
 
  export default async function createP2PTransaction (amount : string, number: string ) {
     const session = await getServerSession();
-    const transaction = await prisma.$transaction([
-        prisma.user.update({
-            where: {id: session?.user?.id},
-            data: {
-                Balance: {
-                    
-                }
-            }
-        }),
+    
+    // Getting id from session object for current user / sender
+    const from = session?.user.id
+    // Checking if logged in
+    if (!from) {
+        return {
+            message: "Not a valid request"
+        }
+    }
 
-    ])
- }
+    // getting receiver using number and checking for existence
+    const to = await prisma.user.findUnique({
+        where: {
+            number: number
+        },
+        select: {
+            id: true
+        }
+    })
+    
+    if (!to?.id) {
+        return {
+            message : "Receiver not found"
+        }
+    }
+    
+    return prisma.$transaction( async (tx) => {
+        // Locking rows when performing transactions rather than
+        // const fromBalance = await tx.balance.findUnique({where: {userId: from,}}) 
+
+        const fromBalance = await prisma.$queryRaw<Balance>`SELECT * FROM "Balance" WHERE "userId" = ${from} FOR UPDATE;`
+
+        if ( !fromBalance || fromBalance?.amount < Number(amount)) {
+            return {
+                message: "Insufficient Balance"
+            }
+        }
+
+        // Following 2 can lead to negative balance in race conditions
+        // if where: { amount: fromBalance.amount } -- is not used in update balance of "from"
+        // this is called Optimistics Concurrency Control
+        // an alternative of LOCKING for cross DB usage + moderate concurrency
+
+        await tx.balance.update({
+            where: {userId: to.id},
+            data: {amount: {increment: Number(amount)}}
+        })
+
+        await tx.balance.update({
+            where: {userId : from, amount: fromBalance.amount},
+            data: {amount: {decrement: Number(amount)}}
+        })
+
+        await tx.p2pTransfer.create({
+            data: {
+                toUserId: to.id,
+                fromUserId: from,
+                timestamp: new Date(),
+                amount: Number(amount)
+            }
+        })
+    })
+}
+ 
