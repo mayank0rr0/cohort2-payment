3cf5dd3cc949a3a9018deeca3db2f9176e8b358b
Mayank K <mayank0rr0@gmail.com>
2025-11-10 18:13:57 +0000

    Modified App Bar with Username & More button

--- apps/user-app/AppBarClient.tsx
+++ apps/user-app/AppBarClient.tsx
@@ -1,12 +1,14 @@
 "use client"
 import AppBar from "@repo/ui/AppBar";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 export const AppBarCliemt = () => {
     const session = useSession();
+    const [show, setShow] = useState(false)
 
     return <div>
-        <AppBar onSignin={signIn} onSignout={async () => {
+        <AppBar onClick={setShow} show={show} onSignin={signIn} onSignout={async () => {
             signOut({callbackUrl: "/api/auth/signin"})
         }} user={session.data?.user} />
     </div>
