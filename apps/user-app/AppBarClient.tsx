b559cbbf7db597ae2ba32d31bd21b2e72ccba0e8
Mayank K <mayank0rr0@gmail.com>
2025-11-12 18:00:30 +0000

    Modified UI & Added Logo

--- apps/user-app/AppBarClient.tsx
+++ apps/user-app/AppBarClient.tsx
@@ -1,6 +1,7 @@
 "use client"
 import AppBar from "@repo/ui/AppBar";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import { useState } from "react";
 
 export const AppBarCliemt = () => {
@@ -10,6 +11,13 @@ export const AppBarCliemt = () => {
     return <div>
         <AppBar onClick={setShow} show={show} onSignin={signIn} onSignout={async () => {
             signOut({callbackUrl: "/api/auth/signin"})
-        }} user={session.data?.user} />
+        }} user={session.data?.user} img={<Logo />} />
+    </div>
+}
+
+const Logo = () => {
+    return <div>
+        <Image className="dark:hidden" src="/images/light-long-hq.png" alt="logo" width={180} height={100} />
+        <Image className="not-dark:hidden" src="/images/dark-long-hq.png" alt="logo" width={180} height={100} />
     </div>
 }
