96fac63b956d0c745968ea7a21dc9339886e7901
Mayank K <mayank0rr0@gmail.com>
2025-11-16 18:05:03 +0000

    feat: Modify UI and dark theme

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import '@repo/ui/styles.css';
 import { Provider } from "../providers";
-import { AppBarCliemt } from "../AppBarClient";
 import { ThemeProvider } from "../components/ThemeProvider";
 
 const geistSans = localFont({
@@ -16,8 +15,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "PayMe App",
-  description: "Simple wallet app",
+  title: "Simple Payment",
+  description: "A simple wallet app",
 };
 
 export default async function RootLayout({
@@ -32,12 +31,7 @@ export default async function RootLayout({
         <Provider>
           <ThemeProvider>
             <div className="flex flex-col w-full dark:text-zinc-200">
-              <AppBarCliemt />
-              <div className="flex flex-row h-full"> 
-                <div className="w-full">
-                  {children}
-                </div>
-              </div>
+              {children}
             </div>
           </ThemeProvider>
         </Provider>
