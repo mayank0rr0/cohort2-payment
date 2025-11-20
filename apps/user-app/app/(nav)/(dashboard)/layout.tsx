abf0424ee6d869f630d76d4af2754c95a9132c32
Mayank K <mayank0rr0@gmail.com>
2025-11-20 14:29:07 +0000

    feat: Add redirect to /signin for unauthenticated users

--- apps/user-app/app/(nav)/(dashboard)/layout.tsx
+++ apps/user-app/app/(nav)/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import { StateProvider } from "../../../components/StateProvider";
 import { authOptions } from "../../../lib/auth";
 import { getUser } from "../../../lib/getUser";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 
 interface DashboardLayoutProps {
@@ -14,6 +15,9 @@ export default async function DashboardLayout({children} : DashboardLayoutProps)
     const session = await getServerSession(authOptions);
     const userData = await getUser(session?.user.id ?? '');
 
+    if (session?.user) {
+        redirect('/signin')
+    } 
 
     return <div className="flex h-full">
         <StateProvider tranxAll={userData?.tranxList ?? []} p2p={userData?.p2p ?? []} balance={userData?.data?.Balance[0] ?? null} transactions={userData?.data?.OnRampTransaction ?? null} />
