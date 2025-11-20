cb5ab0ce15ccf7760c79729ae94f342247e1f68d
Mayank K <mayank0rr0@gmail.com>
2025-11-20 14:29:57 +0000

    feat: Add redirect to /dashboard for authenticated users

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,13 +4,12 @@ import { redirect } from "next/navigation";
 import { Landing } from "../components/Landing";
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (session?.user) {
-  //   redirect('/dashboard')
-  // }
+  if (session?.user) {
+    redirect('/dashboard')
+  }
 
-  // OF NO USE THE USER WILL BE REDIRECTED ANYHOW
   return (
   <div className="h-full w-full">
     <Landing />
