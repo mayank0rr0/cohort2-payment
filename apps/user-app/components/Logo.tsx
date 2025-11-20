1e4ea2c16edbbb0aecce58688bff076c40231ef2
Mayank K <mayank0rr0@gmail.com>
2025-11-20 14:23:10 +0000

    feat: Add Link to the Logo

--- apps/user-app/components/Logo.tsx
+++ apps/user-app/components/Logo.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const LogoAbsolute =  () => {
     return <div className="absolute pt-8 px-5 sm:pt-10! md:px-10! z-100 ">
-        <Image className="dark:hidden w-22 md:w-30! lg:w-35!" src="/images/light-lq.png" alt="logo" width={150} height={200} />
-        <Image className="not-dark:hidden w-22 md:w-30! lg:w-35!" src="/images/dark-lq.png" alt="logo" width={150} height={200} />
+        <Link href="/">
+            <Image className="dark:hidden w-22 md:w-30! lg:w-35!" src="/images/light-lq.png" alt="logo" width={150} height={200} />
+            <Image className="not-dark:hidden w-22 md:w-30! lg:w-35!" src="/images/dark-lq.png" alt="logo" width={150} height={200} />
+        </Link>
     </div>
 } 
