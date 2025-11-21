a8e055f0f9cd936acc326f39c5ad4eed832845c1
Mayank K <mayank0rr0@gmail.com>
2025-11-21 12:47:07 +0000

    feat: Add probabiity logic for status to success and failure

--- apps/bank_webhook_handler/src/index.ts
+++ apps/bank_webhook_handler/src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
+
+    const prob = Math.floor(Math.random() * 100) 
+
     // add zod validation
     const paymentInfo: {
         token : string,
@@ -18,36 +21,58 @@ app.post("/hdfcWebhook", async (req, res) => {
         amount : req.body.amount
     };
     // update balance in db, add txn 
-    try {
-        await prisma.$transaction([
-            prisma.onRampTransaction.updateMany({
-                where: {
-                    token : paymentInfo.token
-                },
-                data: {
-                    status: "Success"
-                }
-            }),
-            prisma.balance.updateMany({
-                where: {
-                    userId: paymentInfo.userId
-                },
-                data : {
-                    amount: {
-                        increment: paymentInfo.amount
+    if (prob <= 65 ) {
+        try {
+            await prisma.$transaction([
+                prisma.onRampTransaction.updateMany({
+                    where: {
+                        token : paymentInfo.token
+                    },
+                    data: {
+                        status: "Success"
                     }
-                }
-            })
-        ]);
+                }),
+                prisma.balance.updateMany({
+                    where: {
+                        userId: paymentInfo.userId
+                    },
+                    data : {
+                        amount: {
+                            increment: paymentInfo.amount
+                        }
+                    }
+                })
+            ]);
 
-        res.json({
-            message: "Captured"
-        }) 
-    } catch (e) {
-        console.error(e);
-        res.status(411).json({
-            message: "Error while processing webhook"
-        })
+            res.json({
+                message: "Captured"
+            }) 
+        } catch (e) {
+            console.error(e);
+            res.status(411).json({
+                message: "Error while processing webhook"
+            })
+        }
+    } else {
+        try {
+            await prisma.onRampTransaction.updateMany({
+                    where: {
+                        token : paymentInfo.token
+                    },
+                    data: {
+                        status: "Failure"
+                    }
+                });
+
+            res.json({
+                message: "Captured"
+            }) 
+        } catch (e) {
+            console.error(e);
+            res.status(411).json({
+                message: "Error while processing webhook"
+            })
+        }
     }
 })
 
